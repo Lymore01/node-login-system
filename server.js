11ddb32976740933dbf4033ff48e514b175f28fa
Lymore01 <kellytoroitich89@gmail.com>
2024-04-02 10:14:30 +0300

    Added nodemailer

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const taskRoute = require("./routes/tasks")
 const userRoute = require("./routes/users")
 const session = require("express-session")
 const store = require("./database/SessionStorage/sessions")
+const nodemailer = require("nodemailer")
 
 
 // create session in the server
@@ -30,6 +31,42 @@ app.use("/task", taskRoute)
 app.use("/user", userRoute)
 
 
+// nodemailer
+
+const transporter = nodemailer.createTransport({
+    service:'Gmail',
+    auth:{
+        user:'kellytoroitich89@gmail.com',
+        pass:process.env.EMAIL_PASS
+    }
+})
+
+const mailOptions = {
+    from:'kellytoroitich89@gmail.com',
+    to:'kellytoroitich89@gmail.com',
+    subject:'Test Email',
+    text:'This is a test email from Node.js using Nodemailer.'
+}
+
+// send email
+transporter.sendMail(mailOptions, (error, info)=>{
+    if (error){
+        console.error("Error sending email:", error)
+    }else{
+        console.log('Email sent:', info.response)
+    }
+})
+app.get("/email", (req,res)=>{
+    transporter.sendMail(mailOptions, (error, info)=>{
+        if (error){
+            res.status(400).json({message:"Error sending email"})
+            console.error("Error sending email:", error)
+        }else{
+            res.status(200).json({message:"Email sent"})
+            console.log('Email sent:', info.response)
+        }
+    })
+})
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`)
